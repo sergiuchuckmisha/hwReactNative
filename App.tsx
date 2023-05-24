184609c255e2a0b1caa041299285520ad779c940
Misha <sergiuchuckmisha@gmail.com>
2023-05-25 00:13:23 +0300

    seems workable; ready for TestFlight

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { Text, View, StyleSheet, TextInput, Button, ScrollView } from 'react-nat
 import {serverUrlWs} from "./src/utils";
 
 
+// code is taken (and edited) from
+// https://blog.logrocket.com/how-to-implement-websockets-in-react-native/
 export default function App() {
     const [serverState, setServerState] = React.useState('Loading...');
     const [messageText, setMessageText] = React.useState('');
@@ -13,10 +15,12 @@ export default function App() {
 
 
     React.useEffect(() => {
-        const serverMessagesList = [];
+         // serverMessagesList = [];
         ws.onopen = () => {
             setServerState('Connected to the server')
             setDisableButton(false);
+            console.log("initializing global.serverMessagesList")
+            global.serverMessagesList = [];
         };
         ws.onclose = (e) => {
             setServerState('Disconnected. Check internet or server.')
@@ -26,14 +30,33 @@ export default function App() {
             setServerState(e.message);
         };
         ws.onmessage = (e) => {
-            serverMessagesList.push(e.data);
-            setServerMessages([...serverMessagesList])
+            console.log("onmessage: global.serverMessagesList before push: ", global.serverMessagesList)
+            global.serverMessagesList.push(e.data);
+            console.log("onmessage: global.serverMessagesList after push: ", global.serverMessagesList)
+            setServerMessages([...global.serverMessagesList])
         };
     }, [])
+
+    const printServerMessages = () => {
+        console.log("global.serverMessagesList: ", global.serverMessagesList)
+        console.log("serverMessages: ", serverMessages)
+    }
     const submitMessage = () => {
+        // setServerMessages([messageText])
         ws.send(messageText);
         setMessageText('')
         setInputFieldEmpty(true)
+
+
+        console.log("messageText: ", messageText)
+        console.log("serverMessages: ", serverMessages)
+        // let serverMessagesList = [...serverMessages]
+        console.log("serverMessagesList: ", global.serverMessagesList)
+        global.serverMessagesList.push(messageText);
+        console.log("global.serverMessagesList after push: ", global.serverMessagesList)
+        setServerMessages([...global.serverMessagesList])
+        console.log("serverMessages after set: ", serverMessages)
+        console.log("messageText after: ", messageText)
     }
     return (
         <View style={styles.container}>
@@ -43,21 +66,6 @@ export default function App() {
             padding: 5
     }}>
 <Text>{serverState}</Text>
-    </View>
-    <View style={{
-        backgroundColor: '#ffeece',
-            padding: 5,
-            flexGrow: 1
-    }}>
-<ScrollView>
-    {
-        serverMessages.map((item, ind) => {
-            return (
-                <Text key={ind}>{item}</Text>
-        )
-        })
-    }
-    </ScrollView>
     </View>
     <View style={{
         flexDirection: 'row',
@@ -80,8 +88,36 @@ export default function App() {
     title={'Submit'}
     disabled={disableButton || inputFieldEmpty}
     />
+    <Button
+    onPress={() => {
+        console.log("initializing global.serverMessagesList")
+        global.serverMessagesList = [];
+        setServerMessages([])
+    }}
+    title={'Clear chat'}
+    disabled={disableButton}
+    />
+    <Button
+    onPress={() => printServerMessages()}
+    title={'Print'}
+    disabled={disableButton}
+    />
     </View>
-
+            <View style={{
+                backgroundColor: '#ffeece',
+                padding: 5,
+                flexGrow: 1
+            }}>
+                <ScrollView>
+                    {
+                        serverMessages.map((item, ind) => {
+                            return (
+                                <Text key={ind}>{item}</Text>
+                            )
+                        })
+                    }
+                </ScrollView>
+            </View>
     </View>
 );
 }
