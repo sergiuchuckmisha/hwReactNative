dfa4dc064e2300478305cbf0fda517e1800c2b55
Misha <sergiuchuckmisha@gmail.com>
2023-05-24 18:01:50 +0300

    move websocket initialization to utils

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -1,60 +1,22 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {StyleSheet, Text, View} from "react-native";
 import {Colors} from "react-native/Libraries/NewAppScreen";
-// import {initWssConnection} from "./ws";
-// import {tmp} from "./ws";
-import axios from "axios/index";
-import {compareArrays, httpCall} from "./utils";
-import WebSocket from "isomorphic-ws";
+import {initWebSocketConnection} from "./utils";
+
 
 
 const Messages = ({messages = [], style = {}}) => {
     const [messages_, setMessages_] = useState(messages);
-    // console.log("Messages: tmp: ", tmp)
-    console.log("Messages: axios: ", axios)
-    console.log("Messages: axios.get: ", axios.get)
-    // NetInfo.fetch().then(state => {
-    //     console.log("Connection type", state.type);
-    //     console.log("Is connected?", state.isConnected);
-    // });
 
-    httpCall(data => {
-        console.log("onfulfilled: typeof [r.data]: ", typeof [data])
-        console.log("onfulfilled: typeof [r.data]: ", typeof [data])
-        console.log("onfulfilled: [r.data] instanceof Array: ", [data] instanceof Array)
-        // console.log("[r.data].equals: ", [data].equals)
-        if (!compareArrays([data], messages_)) {
-            setMessages_([data])
-            alert(1)
-        }
-    });
-
-    let wss = new WebSocket(serverUrl)
-    console.log("wss: ", wss)
-    wss.onopen = () => wss.send("ping")
-    wss.onmessage = ({ data }) => {
-        try {
+    useEffect(() => {
+        console.log("hello from useEffect; starting initWssConnection")
+        initWebSocketConnection(message => {
             // alert(data)
-            console.log(data)
-            // if ('function' === typeof f) {
-            //     f(data)
-            // }
-        } catch (e) { // usually this error should be processing of response to 'ping' (which is required in order to keeping subscription alive)
-            console.log('error during processing message', data)
-            console.log(e)
-        }
-    };
-
-    // alert("tmp: " + tmp)
-    // initWssConnection()
-    // useEffect(() => {
-    //     console.log("hello from useEffect; starting initWssConnection")
-    //     initWssConnection(m => {
-    //     console.log("hello from useEffect; messages_ before: ", messages_)
-    //     let newMessages = messages_.concat([m])
-    //     console.log("hello from useEffect; messages_ after: ", newMessages)
-    //     setMessages_(newMessages)})
-    // }, [])
+            let newMessages = messages_.concat([message])
+            console.log("hello from useEffect; messages_ after: ", newMessages)
+            setMessages_(newMessages)
+        });
+    }, [])
 
     return (<View style={style}>
         <Text>"asdasd"</Text>
@@ -94,40 +56,3 @@ const styles = StyleSheet.create({
 });
 
 export default Messages;
-
-
-const serverUrl = "ws://206.189.101.71:8088"
-export function initWssConnection (f) {
-    console.log("hello from initWssConnection; WebSocket: ", WebSocket)
-    console.log("hello from initWssConnection; new WebSocket(serverUrl): ", new WebSocket())
-    const wss = new WebSocket(serverUrl)
-    console.log('initializing wss connection at', new Date());
-    console.log(wss)
-    global.wss = wss;
-    //
-    // wss.onopen = () => {
-    //     wss.send("ping");
-    // };
-
-    // wss.onmessage = ({ data }) => {
-    //     try {
-    //         // alert(data)
-    //         if ('function' === typeof f) {
-    //             f(data)
-    //         }
-    //     } catch (e) { // usually this error should be processing of response to 'ping' (which is required in order to keeping subscription alive)
-    //         console.log('error during processing message', data)
-    //         console.log(e)
-    //     }
-    // };
-    //
-    // wss.onclose = function close() {
-    //     console.log('disconnected');
-    //     if (global.shouldBeAlive) {
-    //         console.log('trying to reconnect at', new Date());
-    //         initWssConnection()
-    //     }
-    // };
-
-    return wss
-}
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import WebSocket from "isomorphic-ws";
+
+export const serverUrl = "ws://206.189.101.71:8088"
 
 export function compareArrays(ar1: any[], ar2: any[]) {
     return JSON.stringify(ar1) === JSON.stringify(ar2);
@@ -7,4 +10,23 @@ export function compareArrays(ar1: any[], ar2: any[]) {
 export function httpCall(onfulfilled: (data: any) => void) {
     axios.get(`http://206.189.101.71:8080/`)
         .then(r => onfulfilled(r.data))
+}
+
+export function initWebSocketConnection(onMessage: (data: any) => void) {
+    let wss = new WebSocket(serverUrl)
+    console.log("wss: ", wss)
+    wss.onopen = () => wss.send("ping")
+    wss.onmessage = ({data}) => {
+        try {
+            // alert(data)
+            console.log(data)
+            onMessage(data)
+            // if ('function' === typeof f) {
+            //     f(data)
+            // }
+        } catch (e) { // usually this error should be processing of response to 'ping' (which is required in order to keeping subscription alive)
+            console.log('error during processing message', data)
+            console.log(e)
+        }
+    };
 }
